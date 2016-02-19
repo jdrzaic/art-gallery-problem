01d666068680636782e080632448e7501a9036aa
Jelena Drzaic <jelena.drzaic1@gmail.com>
2016-02-19 03:19:34 +0100

    merge genetic

--- src/main/java/metaheuristics/project/agp/alg/genetic/GeneticAlgorthm.java
+++ src/main/java/metaheuristics/project/agp/alg/genetic/GeneticAlgorthm.java
@@ -1,6 +1,7 @@
 package metaheuristics.project.agp.alg.genetic;
 
 import java.io.File;
+import java.io.*;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
@@ -31,22 +32,33 @@ public class GeneticAlgorthm {
 	 * @return number of cameras
 	 */
 	public int process(String filePolygon, String fileToSave, String initCover) {
-		System.out.println("genetic calld");
 		try {
-			String execName = System.getProperty("os.name").startsWith("Windows") ? "./GeneticAlgorithm.exe" : "./GeneticAlgorithm";
-			Process p = Runtime.getRuntime().exec(execName + " " +  
-				  filePolygon  + " " + initCover + " " + fileToSave);
+
+			String os = System.getProperty("os.name");
+			String execName = "";
+			String cmd = "";
+			if(os.startsWith("Windows")) {
+				execName = "./GeneticAlgorithm.exe";
+				String maybeInitCover = initCover.isEmpty() ? "" : "\"" + initCover + "\"";
+				cmd = execName + " \"" + filePolygon  + "\" " + maybeInitCover + " \"" + fileToSave + "\"";
+			} else {
+				execName = "./GeneticAlgorithm";
+				cmd = execName + " " + filePolygon  + " " + initCover + " " + fileToSave;
+			}
+			System.out.println(cmd);
+			Process p = Runtime.getRuntime().exec(cmd);
 			try {
 				p.waitFor();
+				copy(p.getInputStream(), System.out);
+				copy(p.getErrorStream(), System.out);
 			} catch (InterruptedException e) {
 				System.err.println("Error wainting bash");
 			}
 		} catch (IOException e) {
 			System.err.println("Error executing bash");
 		}
-		System.out.println("genetic ended");
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {}
 		int n = 0;
 		try {
@@ -58,4 +70,12 @@ public class GeneticAlgorthm {
 		}
 		return n;
 	}
+
+	  static void copy(InputStream in, OutputStream out) throws IOException {
+	    while (true) {
+	      int c = in.read();
+	      if (c == -1) break;
+	      out.write((char)c);
+	    }
+	  }
 }
